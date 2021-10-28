6244636a4102ecb6cd8b037d924ce4d83edaef31
moisesilva <moisesmarques.ti@hotmail.com>
2021-10-28 16:28:59 -0300

    Slider positioning, transition and additional info

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,12 +2,14 @@ import './Slider.css';
 import React, { useState, useEffect } from 'react';
 import GetObject from '../services/GetObjectService';
 
+const slide_interval= 10000;
+
 function Slider({ items }) {
 
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [item, setItem] = useState(null);
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(100000);
   const [play, setPlay] = useState(true);
 
   function playStop() {
@@ -21,6 +23,13 @@ function Slider({ items }) {
       .then(res => res.json())
       .then(
         (result) => {
+          if(!result.primaryImage)
+          {
+            console.log('no image. skipping.')
+            setCount(c => c + 1);
+            return;
+          }
+
           setIsLoaded(true);
           setItem(result);
           console.log(result);
@@ -35,7 +44,7 @@ function Slider({ items }) {
       fetchData();
       const id = setInterval(() => {
         setCount(c => c + 1);
-      }, 3000);
+      }, slide_interval);
       return () => clearInterval(id);
     }
 
@@ -45,11 +54,20 @@ function Slider({ items }) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
     return <div>Loading...</div>;
-  } else if (item) {
+  } else if (item && play) {
     return (
       <div className="Slider" onClick={playStop} >
-        <h1>{item.title}</h1>
-      
+        <img src={item.primaryImage} alt={item.title} className="primaryImage"/>
+      </div>
+    );
+  } else if (item && !play) {
+    return (
+      <div className="Slider" onClick={playStop} >
+        <h1 className="title">{item.title}</h1>
+          <img src={item.primaryImage} alt={item.title} className="primaryImage"/>
+        <h2 className="creditLine">{item.creditLine}</h2>
+        <h3 className="repository">{item.repository}</h3>
+        <p className="dimensions">{item.dimensions}</p>
       </div>
     );
   } else
